83bb68b0aaa409811ccdd200c9145179b12d0b77
Chandler Burton <chandlerburton@chandlers-mbp.lan>
2020-07-28 09:53:37 -0700

    Added search functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from markdown2 import Markdown
 
@@ -6,9 +7,21 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+                q = request.GET.get('q')
+                if q is None:
+                        return render(request, "encyclopedia/index.html", {
+                        "entries": util.list_entries()
+                    })
+                elif q is not None:
+                        results = util.get_entry(q)
+                        if results:
+                            return HttpResponseRedirect(f'wiki/{q}')
+                        elif results is None:
+                            entries = util.list_entries()
+                            entries = [i for i in entries if i.lower().find(q.lower()) != -1]
+                            return render(request, 'encyclopedia/search.html', {
+                                'entries': entries
+                            })
 
 def show_page(request, entry):
 
